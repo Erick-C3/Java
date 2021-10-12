b30f3380537f2b118f4b88309eec29b0aedceaef
Erick-C3 <81482790+Erick-C3@users.noreply.github.com>
2021-10-12 12:30:34 -0300

    Add files via upload

---
+++ HelloWorldJava/src/helloworldjava/HelloWorldJava.java
@@ -0,0 +1,12 @@
+
+package helloworldjava;
+
+
+public class HelloWorldJava {
+
+   
+    public static void main(String[] args) {
+        System.out.println("Hello world!"); // cout y printf
+    }
+    
+}
